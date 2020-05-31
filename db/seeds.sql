4c3c123f8818f6770821be114c266e69488fd84a
Karthik Kovi <karthi.kovi@gmail.com>
2020-06-01 06:38:16 +0930

    Updated controllers, db and models

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(  burger_name,devoured)VALUES("Burger1",false);
+INSERT INTO burgers(  burger_name,devoured)VALUES("Burger2",false);
+INSERT INTO burgers(  burger_name,devoured)VALUES("Burger3",false);
+INSERT INTO burgers(  burger_name,devoured)VALUES("Burger4",false);
